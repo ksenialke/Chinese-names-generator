c2a1e40b41f6f9f6683b37ae40f693efa33de80f
Ksenia Klamut <ksenia.klamut@gmail.com>
2019-03-20 21:35:16 +0100

    improved asynchronous function and added getSyllable(arr);

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 const fs = require('fs');
-var content;
 fs.readFile('syllables.txt', "utf8", function read(err, data) {
     if (err) {
         throw err;
     }
-    content = data;
-    processFile();
+    processFile(data);
 });
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function processFile() {
+function getSyllable(arr){
+    return arr[Math.floor(Math.random() * arr.length)]
+}
+
+function processFile(data) {
     var surname;
     var name;
-    var lines = content.split('\n');
+    var lines = data.split('\n');
     for (var line = 0; line < lines.length; line++) {
         lines[line] = lines[line].substring(1, lines[line].length - 1);
     }
-    surname = capitalizeFirstLetter(lines[Math.floor(Math.random() * lines.length)]);
-    name = capitalizeFirstLetter(lines[Math.floor(Math.random() * lines.length)] + lines[Math.floor(Math.random() * lines.length)]);
+    surname = capitalizeFirstLetter(getSyllable(lines));
+    name = capitalizeFirstLetter(getSyllable(lines) + getSyllable(lines));
     console.log(`${surname} ${name}`)
 }
